dd59fbd894989d720b3cd1d8db83e42a61bbd57a
Gustavo Saavedra <tavito.286@gmail.com>
2022-09-20 19:05:01 -0500

    Agrega funcion para despedir

--- prueba-2.py
+++ prueba-2.py
@@ -1,2 +1,7 @@
-print("hola-2")
-print("how are you two?")
+def despedir_a(nombre):
+    print(f'chao {nombre}')
+
+despedir_a('Juan')
+despedir_a('Emmanuel')
+despedir_a('Luis')
+despedir_a('Gustavo')
